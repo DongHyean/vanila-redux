2fc7e1d2dc827301719ccd174d22984dfd20e478
FGB-DongHyean <gua05062@fgb.ai>
2023-12-22 00:40:15 +0900

    Test

--- src/index.js
+++ src/index.js
@@ -49,83 +49,84 @@
 // });
 
 // #2
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+// const form = document.querySelector("form");
+// const input = document.querySelector("input");
+// const ul = document.querySelector("ul");
 
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
+// const ADD_TODO = "ADD_TODO";
+// const DELETE_TODO = "DELETE_TODO";
 
-const addToDo = (text) => {
-  return { type: ADD_TODO, text };
-};
+// const addToDo = (text) => {
+//   return { type: ADD_TODO, text };
+// };
 
-const deleteToDo = (id) => {
-  return { type: DELETE_TODO, id };
-};
+// const deleteToDo = (id) => {
+//   return { type: DELETE_TODO, id };
+// };
 
-const reducer = (state = [], action) => {
-  //console.log(action);
-  switch (action.type) {
-    case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
-    case DELETE_TODO:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-const store = createStore(reducer);
+// const reducer = (state = [], action) => {
+//   //console.log(action);
+//   switch (action.type) {
+//     case ADD_TODO:
+//       return [...state, { text: action.text, id: Date.now() }];
+//     case DELETE_TODO:
+//       return state.filter((toDo) => toDo.id !== action.id);
+//     default:
+//       return state;
+//   }
+// };
+// const store = createStore(reducer);
 
-store.subscribe(() => {
-  console.log(store.getState());
-});
+// store.subscribe(() => {
+//   console.log(store.getState());
+// });
 
-const dispatchAddToDo = (text) => {
-  store.dispatch(addToDo(text));
-};
+// const dispatchAddToDo = (text) => {
+//   store.dispatch(addToDo(text));
+// };
 
-const dispatchDeleteToDo = (e) => {
-  //console.log("delete");
-  //console.log(e.target.parentNode.id);
+// const dispatchDeleteToDo = (e) => {
+//   //console.log("delete");
+//   //console.log(e.target.parentNode.id);
 
-  const id = parseInt(e.target.parentNode.id);
-  store.dispatch(deleteToDo(id));
-};
+//   const id = parseInt(e.target.parentNode.id);
+//   store.dispatch(deleteToDo(id));
+// };
 
-const paintToDos = () => {
-  const toDos = store.getState();
+// const paintToDos = () => {
+//   const toDos = store.getState();
 
-  ul.innerHTML = "";
+//   ul.innerHTML = "";
 
-  toDos.forEach((toDo) => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", dispatchDeleteToDo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-};
+//   toDos.forEach((toDo) => {
+//     const li = document.createElement("li");
+//     const btn = document.createElement("button");
+//     btn.innerText = "DEL";
+//     btn.addEventListener("click", dispatchDeleteToDo);
+//     li.id = toDo.id;
+//     li.innerText = toDo.text;
+//     li.appendChild(btn);
+//     ul.appendChild(li);
+//   });
+// };
 
-store.subscribe(paintToDos);
+// store.subscribe(paintToDos);
 
-// const createToDo = (toDo) => {
-//   const li = document.createElement("li");
-//   li.innerText = toDo;
-//   ul.appendChild(li);
-// }; => dispatch
+// // const createToDo = (toDo) => {
+// //   const li = document.createElement("li");
+// //   li.innerText = toDo;
+// //   ul.appendChild(li);
+// // }; => dispatch
 
-const onSubmit = (e) => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  //store.dispatch({ type: ADD_TODO, text: toDo });
-  dispatchAddToDo(toDo);
-};
+// const onSubmit = (e) => {
+//   e.preventDefault();
+//   const toDo = input.value;
+//   input.value = "";
+//   //store.dispatch({ type: ADD_TODO, text: toDo });
+//   // 
+//   dispatchAddToDo(toDo);
+// };
 
-form.addEventListener("submit", onSubmit);
+// form.addEventListener("submit", onSubmit);
